f08f1da5c5427930e324c09e30f8fdc1514ef876
Avi-2024 <abis39112@gmail.com>
2026-08-19 20:43:21 +0530

    refactor: add admin resource module boundary

---
+++ physioqr/src/features/admin/resources/index.ts
@@ -0,0 +1,19 @@
+export { adminResourceModules } from './admin-resource.config';
+export type {
+  AdminResourceConfig,
+  AdminResourceDrawerMode,
+  AdminResourceKey,
+  ApiRecord,
+  DrawerState,
+} from './admin-resource.types';
+export {
+  asRecord,
+  displayValue,
+  extractItems,
+  formatAmount,
+  formatDate,
+  getRecordId,
+  getValue,
+  recordObjectId,
+  toDateInputValue,
+} from './admin-resource.utils';
