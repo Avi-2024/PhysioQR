feb0c75cdf69bc7abbeeadb962b64626d883e61b
Avi Tamrakar <abis39112@gmail.com>
2026-08-20 23:33:26 +0530

    fix logo size

--- physioqr/src/components/brand/Logo.tsx
+++ physioqr/src/components/brand/Logo.tsx
@@ -1,71 +1,46 @@
-import React from 'react';
+import React from "react";
 
 interface LogoProps {
   width?: number;
   height?: number;
-  withText?: boolean;
   className?: string;
   textClassName?: string;
-  imageScale?: number;
 }
 
 export function Logo({
-  width = 48,
-  height = 48,
-  withText = true,
-  className = '',
-  textClassName = '',
-  imageScale = 1,
+
 }: LogoProps) {
   return (
     <div
-      className={className}
       style={{
-        display: 'inline-flex',
-        alignItems: 'center',
-        gap: '8px',
-        flexShrink: 0,
+        display: "flex",
+        alignItems: "center",
+        gap: "4px",
       }}
     >
-      <span
+      <img
+        src="/PhysioQR.png"
+        alt="PhysioQR logo"
         style={{
-          width,
-          height,
-          display: 'inline-flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          overflow: 'hidden',
+          width: "56px",
+          height: "56px",
+          objectFit: "contain",
+          display: "block",
           flexShrink: 0,
         }}
-      >
-        <img
-          src="/PhysioQR.png"
-          alt="PhysioQR logo"
-          style={{
-            width: `${width}px`,
-            height: `${height}px`,
-            objectFit: 'contain',
-            display: 'block',
-            transform: `scale(${imageScale})`,
-          }}
-        />
-      </span>
+      />
 
-      {withText && (
-        <span
-          className={textClassName}
-          style={{
-            fontSize: '24px',
-            fontWeight: 800,
-            lineHeight: 1,
-            letterSpacing: '-0.5px',
-            whiteSpace: 'nowrap',
-            color: '#168C45',
-          }}
-        >
-          PhysioQR
-        </span>
-      )}
+      <span
+        style={{
+          fontSize: "22px",
+          fontWeight: 800,
+color: '#169B45',
+          lineHeight: 1,
+          whiteSpace: "nowrap",
+        }}
+      >
+        PhysioQR
+      </span>
     </div>
   );
 }
